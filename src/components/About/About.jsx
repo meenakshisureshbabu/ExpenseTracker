1929e06b1ba1b40c7538d7a6e0b480b6dc51ab42
meenakshisureshbabu <meenakshi.bca@gmail.com>
2024-01-21 15:49:09 -0500

    Adding About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,12 +9,18 @@ function About() {
     <div className='main-about-div'>
         <div  className='about-div'>
         <div>
+            <div className='img-div'>
+            <img className='logo-img' src="https://media.freshbooks.com/wp-content/uploads/2022/02/expense-tracking.jpg" alt="Logo"></img>
+            </div>
+        </div>
+        <div className='text-div'>
             <div>
-            <img className='logo-img' src="/xpenz.png" alt="Logo"></img>
+              <p>Expense tracking helps your business by allowing you to identify and manage spending in an efficient time frame. It's essential to be aware of your business's cash flows so you can note any areas of excessive or inefficient spending.</p>
+              <p><h2>Use our App today to feel better about your future.</h2></p>
             </div>
         </div>
         <div>
-          
+
         </div>
         </div>
     </div>
