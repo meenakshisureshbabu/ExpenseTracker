f3f504c4b58fc64395d947c82e8fbb01eac58944
meenakshisureshbabu <meenakshi.bca@gmail.com>
2024-01-22 10:48:18 -0500

    Adding Contact form

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -7,11 +7,14 @@ function ContactUs() {
     <div>
       <Header />
       <div className="main-div">
+        <div>
+          <img src="https://media.istockphoto.com/id/1441262452/photo/communication-and-technology-concept-hand-putting-wooden-block-cube-symbol-telephone-email.webp?b=1&s=170667a&w=0&k=20&c=FtxQlZeGn__5ZHpc5zi9tx0GVTDZuZQoQcHT7mxDY4Q="/>
+        </div>
       <div className="contact">
         <div className="contact-div">
-          <h3>Meenakshi Suresh Babu</h3>
-          <h3>📞 : 000-000-0000</h3>
-          <h3>📧 : meena@gmail.com </h3>
+          <input type="text" placeholder="Your Name"></input>
+          <input type="email" placeholder="Your Email"></input>
+          <button className="send-button">Send Query</button>
         </div>
         <div className="social">
         <a href="https://facebook.com" target="_blank">
