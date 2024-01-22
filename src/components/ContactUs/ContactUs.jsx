8ebcad132fde54265270d451e838e87285bde607
meenakshisureshbabu <meenakshi.bca@gmail.com>
2024-01-22 11:20:25 -0500

    Adding contact form

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,20 +1,52 @@
 import React from "react";
 import Header from "../Header/Header";
 import "../ContactUs/contactus.css";
+import emailjs from "@emailjs/browser";
 
 function ContactUs() {
+  
+
+  const sendEmail = () => {
+
+    let email = document.getElementById("contact_email").value;
+    let name = document.getElementById("contact_name").value;
+    
+    var templateparams = {
+        contact_email:email,
+        contact_name:name
+    }
+    
+    emailjs
+      .send(
+        "service_gjxr5tm",
+        "template_fyvzhrw",
+        templateparams,
+        "-yjiSbdT-JRQvkpJz"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+
   return (
     <div>
       <Header />
       <div className="main-div">
         <div>
-          <img src="https://media.istockphoto.com/id/1441262452/photo/communication-and-technology-concept-hand-putting-wooden-block-cube-symbol-telephone-email.webp?b=1&s=170667a&w=0&k=20&c=FtxQlZeGn__5ZHpc5zi9tx0GVTDZuZQoQcHT7mxDY4Q="/>
+          <img className="contact-img" src="https://media.istockphoto.com/id/1441262452/photo/communication-and-technology-concept-hand-putting-wooden-block-cube-symbol-telephone-email.webp?b=1&s=170667a&w=0&k=20&c=FtxQlZeGn__5ZHpc5zi9tx0GVTDZuZQoQcHT7mxDY4Q="/>
         </div>
       <div className="contact">
         <div className="contact-div">
-          <input type="text" placeholder="Your Name"></input>
-          <input type="email" placeholder="Your Email"></input>
-          <button className="send-button">Send Query</button>
+          <input type="text" placeholder="Your Name" id="contact_name"></input>
+          <input type="email" placeholder="Your Email" id="contact_email"></input>
+          <button className="send-button" onClick={sendEmail}>Send Query</button>
         </div>
         <div className="social">
         <a href="https://facebook.com" target="_blank">
